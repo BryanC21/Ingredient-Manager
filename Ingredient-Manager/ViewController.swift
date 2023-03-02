8f2578586ddfe4d7427660af27c3c735db380a9b
Bryan Caldera <bryanicaldera@gmail.com>
2023-03-01 16:50:21 -0800

    Initial Commit

---
+++ Ingredient-Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ingredient-Manager
+//
+//  Created by Bryan Caldera on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
